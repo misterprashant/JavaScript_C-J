fc3de23f935b7ff6c3606d633b1bd46602989da7
Prashant Shukla <144648863+misterprashant@users.noreply.github.com>
2025-01-05 17:56:27 +0000

    Variables and Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 145586
+let accountEmail = "hustler8303@gmail.com"
+var accountPassword = "123456"
+accountCity = "Noida"
+
+// accountId = 2
+
+accountEmail = "fhjkf@gmail.com"
+accountPassword = "586553"
+accountCity = "Lucknow"
+let accountState;
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3)  // we are using nodeJS, not browser
+
+console.log(3 
+    +
+        3)  // code readability should be high
+
+console.log("Prashant")
+
+
+let name1 = "Prashant";
+let age = 22
+let isLoggedIn = false
+let state;
+
+//number => 2 to the power 53
+//string => "", ''
+//bigint
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof null);  // object
+console.log(typeof undefined); // undefined
